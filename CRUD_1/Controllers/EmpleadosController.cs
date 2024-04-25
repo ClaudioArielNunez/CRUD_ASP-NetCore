c4374f046b292f8d29c26041585dec5b0ca48126
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-25 17:58:12 -0300

    metodo async Add Post

--- CRUD_1/Controllers/EmpleadosController.cs
+++ CRUD_1/Controllers/EmpleadosController.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUD_1.Data;
+using CRUD_1.Models;
+using CRUD_1.Models.Dominio;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRUD_1.Controllers
 {
     public class EmpleadosController : Controller
     {
+        //Crear y asignar campos de mvcDbContext
+        private readonly MVCDbContext mvcDbContext;
+
+        public EmpleadosController(MVCDbContext mvcDbContext)
+        {
+            this.mvcDbContext = mvcDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmpleadoViewModel nuevo)
+        {
+            var empleado = new Empleado()
+            {
+                Id = Guid.NewGuid(),
+                Nombre = nuevo.Nombre,
+                Email = nuevo.Email,    
+                Salarry = nuevo.Salarry,
+                FechaNacimiento = nuevo.FechaNacimiento,
+                Departamento = nuevo.Departamento
+            };
+            await mvcDbContext.Empleados.AddAsync(empleado);
+            await mvcDbContext.SaveChangesAsync();
+            return RedirectToAction("Add");
         }
     }
 }
--- CRUD_1/Models/Dominio/Empleado.cs
+++ CRUD_1/Models/Dominio/Empleado.cs
@@ -5,7 +5,7 @@ namespace CRUD_1.Models.Dominio
     public class Empleado
     {
         public Guid Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="El campo {0} es requerido")]
         public string Nombre { get; set; }
         public string Email { get; set; }
         public long Salarry { get; set; }
