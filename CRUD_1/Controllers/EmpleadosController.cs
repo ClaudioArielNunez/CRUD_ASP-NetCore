abaf3b1bae0d3d2c6752b1d1fffd2b843a8a3919
ClaudioArielNunez <claudioariel_n@hotmail.com>
2024-04-26 13:05:16 -0300

    Metodo View en controller, vista Detalle

--- CRUD_1/Controllers/EmpleadosController.cs
+++ CRUD_1/Controllers/EmpleadosController.cs
@@ -2,6 +2,8 @@
 using CRUD_1.Models;
 using CRUD_1.Models.Dominio;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
 
 namespace CRUD_1.Controllers
 {
@@ -16,10 +18,19 @@ namespace CRUD_1.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var lista = await mvcDbContext.Empleados.ToListAsync();
+            
+            return View(lista);
+        }
+
+        [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Add(AddEmpleadoViewModel nuevo)
         {
@@ -36,5 +47,26 @@ namespace CRUD_1.Controllers
             await mvcDbContext.SaveChangesAsync();
             return RedirectToAction("Add");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var empleado = await mvcDbContext.Empleados.FirstOrDefaultAsync(x => x.Id == id);
+            if (empleado != null)
+            {
+                var modelo = new UpdateEmpleadoViewModel()
+                {
+                    Id = empleado.Id,
+                    Nombre = empleado.Nombre,
+                    Email = empleado.Email,
+                    Salarry = empleado.Salarry,
+                    FechaNacimiento = empleado.FechaNacimiento,
+                    Departamento = empleado.Departamento
+                };
+                return View(modelo);
+            }
+            return RedirectToAction("Index");            
+        }
+
     }
 }
---
+++ CRUD_1/Models/UpdateEmpleadoViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUD_1.Models
+{
+    public class UpdateEmpleadoViewModel
+    {
+        public Guid Id { get; set; }  //Para el Id de solo lectura
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public long Salarry { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Departamento { get; set; }
+    }
+}
